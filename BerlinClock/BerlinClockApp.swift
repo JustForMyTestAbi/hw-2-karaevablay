6d0de9f94595f2f0c6f603b3931ff10b773a7795
Ablai88 <ablai2602@gmail.com>
2021-11-18 21:10:35 +0600

    Initial Commit

---
+++ BerlinClock/BerlinClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerlinClockApp.swift
+//  BerlinClock
+//
+//  Created by ablai erzhanov on 18.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct BerlinClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
